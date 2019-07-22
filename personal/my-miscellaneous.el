3f444e5047d7439b2ba431ef8095122fa958da4e
rstrudel <rstrudel@gmail.com>
2019-07-22 11:13:53 +0200

    Add preference horizontal splitting

---
+++ personal/my-miscellaneous.el
@@ -0,0 +1,4 @@
+;; Prefer splitting horizontally i.e. windows side-by-side rather than on top
+;; of each other. This is useful for magit status or org-agenda.
+(setq split-height-threshold nil)
+(setq split-width-threshold 20)
