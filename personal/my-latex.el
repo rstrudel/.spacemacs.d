878ba9f0f0a9b8d59303cd4f205e5e20cc2d7d7b
rstrudel <rstrudel@gmail.com>
2019-12-09 17:50:25 +0100

    Remove duplicate or unused imports, set black/white latex preview buffer

--- personal/my-latex.el
+++ personal/my-latex.el
@@ -1,4 +1,8 @@
 ;; full document previews, preview is upadted automatically after recompiling
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
 ;; use luatex
-(setq-default TeX-engine 'luatex)
+;; (setq-default TeX-engine 'luatex)
+
+;; change preview background
+(setq preview-pdf-color-adjust-method nil)
